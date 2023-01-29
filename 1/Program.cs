e1bae4d7d79e74f65017e69e63354d935e4c3d53
Roman <rtkcfc@gmail.com>
2023-01-29 17:40:21 +0600

    first commit

---
+++ 1/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.Write("Введите первое число: ");
+int firstNum = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNum = int.Parse(Console.ReadLine());
+
+if (firstNum > secondNum)
+{
+  Console.WriteLine($"Число {firstNum} больше, чем число {secondNum}" );  
+}
+if (secondNum > firstNum)
+{
+    Console.WriteLine($"Число {secondNum} больше, чем число {firstNum}" );
+}
+
+Console.ReadKey();
---
+++ 2/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.Write("Введите первое число: ");
+int firstNum = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNum = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int thirdNum = int.Parse(Console.ReadLine());
+
+
+
+if(firstNum > secondNum && firstNum > thirdNum)
+{
+    
+    Console.WriteLine($"Число {firstNum} максимальное");
+}
+if(secondNum > firstNum && secondNum > thirdNum)
+{
+    
+    Console.WriteLine($"Число {secondNum} максимальное");
+}
+if(thirdNum > secondNum && thirdNum > firstNum)
+{
+    
+    Console.WriteLine($"Число {thirdNum} максимальное");
+}
+
+Console.ReadKey();
